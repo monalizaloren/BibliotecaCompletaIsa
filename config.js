be6e31bda5d2559b71dfe7954ef6eea185f59e5f
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-06-27 09:40:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,7 @@ import "firebase/firestore";
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB5_2oLtuY6qWkB-X1izT_ofIL1dcoOC7U",
-  authDomain: "bibel-19fb0.firebaseapp.com",
-  projectId: "bibel-19fb0",
-  storageBucket: "bibel-19fb0.appspot.com",
-  messagingSenderId: "116871497083",
-  appId: "1:116871497083:web:d24cdc180fa708202fa9de",
-  measurementId: "G-M5E03X0VSR"
-};
+???????????????????????????????????????
 
 firebase.initializeApp(firebaseConfig);
 
